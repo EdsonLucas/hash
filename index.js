799369bf5a63f45d5209272ec8ef59c4c4450bef
EC2 Default User <ec2-user@ip-172-31-34-244.us-east-2.compute.internal>
2018-06-12 00:33:04 +0000

    add click function

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-let player, pos;
+let player, pos, preenchido;
 
 pos = ['var_1', 'var_2', 'var_3', 'var_4', 'var_5', 'var_6', 'var_7', 'var_8', 'var_9'];
+
+preenchido = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 (function() {
 
@@ -10,25 +12,31 @@ pos = ['var_1', 'var_2', 'var_3', 'var_4', 'var_5', 'var_6', 'var_7', 'var_8', '
         document.getElementById(pos[i]).addEventListener("click", function() {
             document.getElementById(pos[i]).classList.add('p20');
             document.getElementById(pos[i]).innerHTML = player;
+
+            preenchido[i] = 1;
+
+            machine();
         });
     }
-
-    console.log(machine());
 
 })();
 
 function x() {
-    return player = '<div class="xis">x</div>';
+    return '<div class="xis">x</div>';
 }
 
 function o() {
-    return player = '<div class="circulo"></div>';
+    return '<div class="circulo"></div>';
 }
 
 function machine() {
     let machineChoice;
+    do {
+        machineChoice = Math.floor((Math.random()*5));
+    } while(preenchido[machineChoice] == 1);
 
-    machineChoice = Math.floor((Math.random()*8));
+    document.getElementById(pos[machineChoice]).classList.add('p20');
+    document.getElementById(pos[machineChoice]).innerHTML = o();
 
-    return pos[machineChoice];
+    preenchido[machineChoice] = 1;
 }
